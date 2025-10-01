18674c0b969e4389391cf7dae319b2f81f3c4097
Monange <98monange@gmail.com>
2025-10-01 16:39:09 -0700

    constructed the subject and body of the notification mail for HR

--- src/main/java/ca/openbox/process/service/components/EmailNotificationConsumer.java
+++ src/main/java/ca/openbox/process/service/components/EmailNotificationConsumer.java
@@ -5,6 +5,9 @@ import ca.openbox.process.entities.LeaveApplication;
 import ca.openbox.user.repository.UserRepository;
 import org.springframework.stereotype.Component;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 @Component
 public class EmailNotificationConsumer {
     private final WebhookEmailService emailService;
@@ -22,12 +25,26 @@ public class EmailNotificationConsumer {
                 try {
                     LeaveApplication leaveApplication = ApplicationStatusChangeMessageQueue.take();
                     String[] handlers = leaveApplication.getCurrentHandler().split(",");
+                    //1.Retreive the name of applicant
+                    String applicant = leaveApplication.getApplicant();
+                    //2.Construct the subject
+                    String subject =  String.format("Leave Request Submitted – %s", applicant);
+                    //3.Construct the body
+                    //3.1Formatter
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, yyyy h:mm a", Locale.US);
+                    //3.2 the body
+                    String body = String.format(
+                            "%s submitted a leave request from %s to %s. Please log on to https://openbox.brimon.me/ to review it.",
+                            applicant,
+                            leaveApplication.getStart().format(formatter),
+                            leaveApplication.getEnd().format(formatter)
+                    );
                     for (String handler : handlers) {
                         String email = userRepository.getUserDOByUsernameAndActiveIsTrue(handler).getEmail();
                         emailService.sendEmail(
                                 email,
-                                "You have one new leave application to review",
-                                "You have one new leave application to review. Please log on the https://openbox.brimon.me/ to review it."
+                                subject,
+                                body
                         );
                         Thread.sleep(20000);//sleep 20s
                     }
