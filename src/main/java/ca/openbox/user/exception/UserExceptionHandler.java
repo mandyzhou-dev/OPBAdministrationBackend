250928bf9c2183f3a11cd5441d12933bf82fea18
Monange <98monange@gmail.com>
2026-03-03 14:24:33 -0800

    clearException

--- src/main/java/ca/openbox/user/exception/UserExceptionHandler.java
+++ src/main/java/ca/openbox/user/exception/UserExceptionHandler.java
@@ -32,6 +32,6 @@ public class UserExceptionHandler {
                 "message", ex.getMessage()
         );
     }
-    //TODO:InvalidBirthdateException;InvalidSinException;InvalidPhoneNumberException;InvalidPostalCodeException;InvalidProvinceException;PasswordTooWeakException;DuplicateSINException
+    //TODO:InvalidEmail;InvalidBirthdateException;InvalidSinException;InvalidPhoneNumberException;InvalidPostalCodeException;InvalidProvinceException;PasswordTooWeakException;DuplicateSINException
 }
 
